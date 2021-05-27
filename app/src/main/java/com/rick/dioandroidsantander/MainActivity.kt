2e47f9c7c1371e5a30ddf64ac7ef7f9d2f99b27e
ricardvsilva <ricardvsilva@gmail.com>
2021-05-27 16:51:23 -0300

    First Commit class 02 toolbar and colors defined.

---
+++ app/src/main/java/com/rick/dioandroidsantander/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.rick.dioandroidsantander
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+}
